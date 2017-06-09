7cf435e9f2340de39dce14f97d532719ed48efe9
root <root@dev.kingsoft.cn>
2017-06-09 11:45:45 +0800

    获取用户加入的群

--- src/TeamPayload.php
+++ src/TeamPayload.php
@@ -12,4 +12,18 @@ namespace nim;
 class TeamPayload
 {
 
+    private $api;
+
+
+    function __construct($api)
+    {
+        $this->api = $api;
+    }
+
+    public function userJoined($data){
+	$url = Constant::BASE_API.Constant::TEAM_USER_JOINED_ACTION;
+	$result = Http::post($url,$data,$this->api->header);
+	return $result;
+    }
+
 }
